be7783322cf1693e27151923fcb22118f4468e53
David Bousquet <david@programaciondb.cl>
2019-09-27 10:27:21 -0300

    Se agrega contenido a TestEliminar

--- test/modelos/TestEliminar.java
+++ test/modelos/TestEliminar.java
@@ -5,10 +5,27 @@
  */
 package modelos;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  *
  * @author davidbousquet
  */
 public class TestEliminar {
-    
+
+    public TestEliminar() {
+    }
+
+    @Test
+    public void testEliminar() {
+        System.out.println("Eliminar");
+        String id = "Venta001";
+        Venta venta = new Venta("Venta001", 9500, 3);
+        ColeccionVentas instance = new ColeccionVentas();
+        instance.Agregar(venta);
+        boolean esperado = true;
+        boolean obtenido = instance.Eliminar(id);
+        Assert.assertEquals(esperado, obtenido);
+    }
 }
