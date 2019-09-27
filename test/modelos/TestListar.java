5f41f8babeaf2404fd848170cafca40318c415e4
David Bousquet <david@programaciondb.cl>
2019-09-27 10:28:44 -0300

    Se agrega contenido a TestListar

--- test/modelos/TestListar.java
+++ test/modelos/TestListar.java
@@ -5,10 +5,27 @@
  */
 package modelos;
 
+import java.util.ArrayList;
+import junit.framework.Assert;
+import org.junit.Test;
+
 /**
  *
  * @author davidbousquet
  */
 public class TestListar {
-    
+
+    public TestListar() {
+    }
+
+    @Test
+    public void testListar() {
+        System.out.println("Listar");
+        ColeccionVentas instance = new ColeccionVentas();
+        ArrayList<Venta> esperado = new ArrayList<>();
+        ArrayList<Venta> obtenido = instance.Listar();
+        Assert.assertEquals(esperado, obtenido);
+
+    }
+
 }
