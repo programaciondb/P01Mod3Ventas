9d4d7ac4a16d41e49ecbcb55986e2577be093cb0
David Bousquet <david@programaciondb.cl>
2019-09-27 10:10:32 -0300

    commit inicial, se crean clases ColeccionVentas y Venta. Se agrega método subTotal a clase ColeccionVentas

---
+++ src/modelos/ColeccionVentas.java
@@ -0,0 +1,46 @@
+package modelos;
+
+import java.util.ArrayList;
+
+public class ColeccionVentas {
+
+    private ArrayList<Venta> base;
+
+    public ColeccionVentas() {
+        this.base = new ArrayList<Venta>();
+    }
+
+    public ArrayList<Venta> Listar() {
+        return this.base;
+    }
+
+    public boolean Agregar(Venta venta) {
+        return this.base.add(venta);
+    }
+
+    public boolean modificar(Venta venta) {
+        for (int i = 0; i < this.base.size(); i++) {
+            if (this.base.get(i).getCodigo().equals(venta.getCodigo())) {
+                this.base.get(i).setCodigo(venta.getCodigo());
+                this.base.get(i).setPrecio(venta.getPrecio());
+                this.base.get(i).setCantidad(venta.getCantidad());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean Eliminar(String codigo) {
+        for (int i = 0; i < this.base.size(); i++) {
+            if (this.base.get(i).getCodigo().equals(codigo)) {
+                this.base.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public int subTotal(int indice){
+        return base.get(indice).getCantidad()*base.get(indice).getPrecio();
+    }
+}
