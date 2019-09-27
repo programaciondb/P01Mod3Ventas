6a322cf1bad6fe0660ce6bad8c15e8a66ed2e631
David Bousquet <david@programaciondb.cl>
2019-09-27 10:31:17 -0300

    Se implementa TestModificar

--- test/modelos/TestModificar.java
+++ test/modelos/TestModificar.java
@@ -5,10 +5,28 @@
  */
 package modelos;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  *
  * @author davidbousquet
  */
 public class TestModificar {
-    
+
+    public TestModificar() {
+    }
+
+    @Test
+    public void testModificar() {
+        System.out.println("Modificar");
+        Venta venta = new Venta("Venta001", 9500, 3);
+        ColeccionVentas instance = new ColeccionVentas();
+        instance.Agregar(venta);
+        boolean esperado = true;
+        venta = new Venta("Venta001", 12800, 5);
+        boolean obtenido = instance.modificar(venta);
+        Assert.assertEquals(esperado, obtenido);
+    }
+
 }
